a17775f0ba830dff40b068a978fb34387a40a428
shett <shetty.manasa10@gmail.com>
2023-02-04 16:49:32 -0500

    firstcommit

---
+++ Factory_DesignPattern/ProfFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_DesignPattern
+{
+    public class ProfFactory
+    {
+        IProfession  profession = null;
+        public IProfession GetProfession(string typeProf)
+        {
+            if(typeProf == null)
+            {
+
+                return null;
+            }
+            switch (typeProf)
+            {
+                case "Engineer":
+                    return new Engineer();
+                    break;
+                case "Doctor":
+                    return new Doctor();
+                    break;
+
+            }
+            return null;
+        }
+        
+
+    }
+}
---
+++ Factory_DesignPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_DesignPattern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ProfFactory profFactory = new ProfFactory();
+
+            IProfession doctor = profFactory.GetProfession("Doctor");
+            doctor.print();
+
+            IProfession engineer = profFactory.GetProfession("Engineer");
+            engineer.print();   
+
+        }
+    }
+}
